9080ed5dbcacf04287e3a78e3025b5368c96aa53
AdityaPatel10 <aditya97556@gmail.com@gmail.com>
2025-10-04 19:58:38 +0530

    Implemented PPR

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -15,3 +15,22 @@ export const STARTUP_QUERY = `*[_type == "startup" && defined(slug.current) && !
   image,
   pitch
 }`;
+
+export const STARTUP_BY_ID_QUERY = `*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id,
+    name,
+    username,
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}`;
