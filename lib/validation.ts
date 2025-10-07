4ece6b29776243a01b655dff6f771bbd4d67958e
AdityaPatel10 <aditya97556@gmail.com@gmail.com>
2025-10-08 04:04:32 +0530

    implemented the create form page and also set the validation errors

---
+++ lib/validation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must contains at least 3 characters" })
+    .max(100),
+  description: z
+    .string()
+    .min(20, { message: "Description must contains at least 20 characters" })
+    .max(500),
+  category: z
+    .string()
+    .min(3, { message: "Category must contains at least 3 characters" })
+    .max(20),
+  link: z
+    .string()
+    .url({ message: "Please enter a valid url " })
+    .refine(async (url) => {
+      try {
+        const res = await fetch(url, { method: "HEAD" });
+        const contentType = res.headers.get("content-type");
+        return contentType?.startsWith("image/");
+      } catch {
+        return false;
+      }
+    }),
+  pitch: z
+    .string()
+    .min(10, { message: "Pitch must contains at least 10 characters" }),
+});
